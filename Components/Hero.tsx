888f4ab1c212fa6391c3b4f5e58da18ea5a32a33
newton786241 <24tamseelanam11@gmail.com>
2025-01-22 11:07:14 +0530

    initial commit

---
+++ Components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return ( 
+        <div className="pb-20 pt-36 text-white text-center">
+            <h1 className="heading">Hero</h1>
+        </div>
+     );
+}
+ 
+export default Hero;
